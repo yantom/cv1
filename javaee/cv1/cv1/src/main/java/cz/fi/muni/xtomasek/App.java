45c71baea18823518977f5354c2ffddbcabcf8f6
Jan Tomasek <xtomasek@nymfe45.fi.muni.cz>
2016-09-20 17:19:55 +0200

    sdfsdf

---
+++ javaee/cv1/cv1/src/main/java/cz/fi/muni/xtomasek/App.java
@@ -0,0 +1,13 @@
+package cz.fi.muni.xtomasek;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello PA165" );
+    }
+}
